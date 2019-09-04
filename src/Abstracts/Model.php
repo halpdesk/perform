122592c053e9d333b38b9b723669cb0b43035e1b
Daniel Leppänen <daniel.leppanen@gmail.com>
2019-09-04 17:08:58 +0200

    Added abstract classes with basic functionality for Model and Queyr

---
+++ src/Abstracts/Model.php
@@ -0,0 +1,451 @@
+<?php
+
+namespace Halpdesk\Perform\Abstracts;
+
+use Illuminate\Support\Collection;
+use Halpdesk\Perform\Exceptions\ModelNotFoundException;
+use Halpdesk\Perform\Exceptions\QueryException;
+use Halpdesk\Perform\Exceptions\NotImplementedException;
+use Halpdesk\Perform\Exceptions\InvalidTypeException;
+use Halpdesk\Perform\Exceptions\AttributeIsNotDefinedException;
+use Halpdesk\Perform\Exceptions\RelationException;
+use Halpdesk\Perform\Exceptions\QueryClassException;
+use Halpdesk\Perform\Contracts\Query as QueryContract;
+use Halpdesk\Perform\Contracts\Model as ModelContract;
+use Carbon\Carbon;
+
+abstract class Model {
+
+    public $dateFormat = 'Y-m-d\TH:i:s.000';
+    static public $query;
+    protected $model;
+    protected $transformer = ArrayTransformer::class;
+    protected $fields = [];
+    protected $casts = [];
+    protected $dates = ['created_at', 'updated_at'];
+    private $relations = [];
+    private $vars = [];
+
+    /**
+     *  Model constructor
+     *  @param  Mixed   $parameters    Array or object which to fill the model fields
+     */
+    public function __construct($parameters = [], $class = null)
+    {
+        if (!is_array($parameters)) {
+            $parameters = (array)$parameters;
+        }
+        if (!empty($class)) {
+            static::setQueryContract($class);
+        }
+        $this->model = get_class($this);
+        $this->fill($parameters);
+    }
+
+    /**
+     * Magic method get
+     */
+    public function __get($key)
+    {
+        return $this->getAttribute($key);
+    }
+
+    /**
+     * Magic method set
+     */
+    public function __set($key, $value)
+    {
+        $this->setAttribute($key, $value);
+    }
+
+    /**
+     * Magic method isset
+     */
+    public function __isset($key)
+    {
+        return isset($this->vars[$key]);
+    }
+
+    /**
+     * Set the query class
+     * @param string $class The class to set as Query Class
+     * @return ModelContract
+    */
+    static public function setQueryContract(string $class) : ModelContract
+    {
+        if (class_exists($class) && in_array(QueryContract::class, class_implements($class))) {
+
+        } else {
+            throw new QueryClassException('query_class_does_not_exist_or_does_not_implement_query_contract: ' . $class);
+        }
+        static::$query = $class;
+
+        return new static();
+    }
+
+    /**
+     * Get all of the models from the repository
+     * @return Collection
+     */
+    static public function all() : Collection
+    {
+        $query = new static::$query;
+        $data = $query->get();
+        $collection = $data->map(function($item, $key) {
+            // If items returned from $query->get() already is a Model, just return it
+            if ($item instanceof ModelContract) {
+                return $item;
+            } else {
+                return (new static($item));
+            }
+        });
+        return $collection;
+    }
+
+    /**
+     * Get the first model from the repository
+     * @return ModelContract|null
+     */
+    static public function first() : ?ModelContract
+    {
+        $query = new static::$query;
+        $model = $query->first();
+        return $model;
+    }
+
+    /**
+     * Get the first model from the repository or throw exception
+     * @throws ModelNotFoundException
+     * @return ModelContract
+     */
+    static public function firstOrFail() : ModelContract
+    {
+        $query = new static::$query;
+        $model = $query->firstOrFail();
+        return $model;
+    }
+
+    /**
+     * Get the model corresonding to the $id from the repository
+     * @param   $id     The identification number of the object to find
+     * @return ModelContract|null
+     */
+    static public function find($id) : ?ModelContract
+    {
+        $query = new static::$query;
+        $model = $query->find($id);
+        return $model;
+    }
+
+    /**
+     * Get the model corresonding to the $id from the repository or throw exception
+     * @param   $id     The identification number of the object to find
+     * @throws ModelNotFoundException
+     * @return ModelContract
+     */
+    static public function findOrFail($id) : ModelContract
+    {
+        $query = new static::$query;
+        $model = $query->findOrFail($id);
+        return $model;
+    }
+
+    /**
+     * Begin querying a model a basic where clause
+     * @return QueryContract
+     */
+    static public function where($key, $value) : QueryContract
+    {
+        $query = new static::$query;
+        $result = $query->where($key, $value);
+        return $result;
+    }
+
+    /**
+     * Begin querying a model with eager loading
+     * @param  Mixed    $relations  Array of relation methods
+     * @return QueryContract
+     */
+    static public function with(array $relations) : QueryContract
+    {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * Eager load relations on the model
+     * @param Array     $relations  Array of relation methods
+     * @return void
+     */
+    public function load(array $relations)
+    {
+        foreach ($relations as $relation) {
+            if (empty($this->relations[$relation]) && method_exists($this, $relation)) {
+                $model = $this->$relation();
+
+                // Only include it if the return is a Model
+                if ($model instanceof ModelContract || $model instanceof Collection) {
+                    $this->relations[$relation] = $model;
+                    $this->$relation = $model;
+                    // $this->vars[$relation] = $model;
+                } else {
+                    throw new RelationException('relation_method_does_not_return_model_or_collection: '.$relation);
+                }
+            }
+        }
+    }
+
+    /**
+     *  Checks wheter a relation is loaded or not
+     * @param String    $relation   The relation to check
+     * @return bool
+     */
+    public function relationLoaded(string $relation) : bool
+    {
+        return isset($this->relations[$relation]);
+    }
+
+    /**
+     * Create a record in the repository
+     *  @param  Mixed   $parameters    Array or object which to fill the model fields
+     */
+    static public function create($parameters) : ModelContract
+    {
+        $query = new static::$query;
+        $model = $query->create($parameters);
+        return $model;
+    }
+
+    /**
+     * Create a model instance without creating a record in the repository
+     *  @param  Mixed   $parameters    Array or object which to fill the model fields
+     *  @return Model
+     */
+    static public function make($parameters) : ModelContract
+    {
+        $model = new static($parameters);
+        return $model;
+    }
+
+    /**
+     * Set a var without checking mutator, cast or format
+     * Supposed to be used specifically within mutator methods
+     * or otherwise within the class itself, why it is protected
+     *
+     * @param string $key    Key in the fields array for the attribute
+     * @param Mixed  $value  Value to set attribute to
+     * @return void
+     */
+    protected function setVar(string $key, $value) : void
+    {
+        $this->vars[$key] = $value;
+    }
+
+    /**
+     * Set a given attribute on the model
+     * @param string $key    Key in the fields array for the attribute
+     * @param Mixed  $value  Value to set attribute to
+     * @return void
+     */
+    public function setAttribute(string $key, $value) : void
+    {
+        $mutatorMethod = 'set'.ucfirst(camel_case($key)).'Attribute';
+
+        // If we have a mutator
+        if (method_exists($this, $mutatorMethod)) {
+            $value = call_user_func([$this, $mutatorMethod], $value);
+        }
+
+        if (in_array($key, $this->fields) || method_exists($this, $key)) {
+
+            // Convert it by casts
+            $this->vars[$key] = $this->convert($key, $value);
+
+        // Same but camel_case. Observe! Do not mindlessly put this as extra operands in the first if-statement
+        // Remember the day when 6 programmers took 15 minutes (1.5hrs) to discuss the term "operand"
+        } else if (in_array(camel_case($key), $this->fields) || method_exists($this, camel_case($key))) {
+
+            // Convert it by casts
+            $this->vars[camel_case($key)] = $this->convert(camel_case($key), $value);
+        }
+    }
+
+    /**
+     * Get a given attribute on the model, cast by the casts definition
+     * @param string    $key   Key in the fields array for the attribute
+     * @throws AttributeIsNotDefinedException
+     * @return Mixed
+     */
+    public function getAttribute(string $key)
+    {
+        $accessorMethod = 'get'.ucfirst(camel_case($key)).'Attribute';
+
+        if ((in_array($key, $this->fields) || method_exists($this, $key)) && array_key_exists($key, $this->vars)) {
+
+            // If we have an accessor
+            if (method_exists($this, $accessorMethod)) {
+                $value = call_user_func([$this, $accessorMethod], $this->vars[$key]);
+            }
+
+            // Convert it by casts
+            $value = $this->convert($key, $value ?? $this->vars[$key]);
+
+            // Format if date
+            if (in_array($key, $this->dates) && ($value instanceof Carbon)) {
+                return $value->format($this->dateFormat);
+            }
+
+            return $value;
+        } else {
+            throw new AttributeIsNotDefinedException('attribute_not_found: '.$key);
+        }
+    }
+
+    /**
+     * Get the date formats array
+     * @return Array    The $date array
+     */
+    public function getDates()
+    {
+        return $this->dates;
+    }
+
+    /**
+     * Update the model in the repository
+     * @param Array     $parameters     Parameters to update model fields with
+     * @return ModelContract
+     */
+    public function update(array $parameters)
+    {
+        $query = new static::$query;
+        $model = $query->update($parameters);
+        return $model;
+    }
+
+    /**
+     * Save the model in the repository
+     * @return bool
+     */
+    public function save() : bool
+    {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * Delete the model from the repository
+     * @return bool
+     */
+    public function delete() : bool
+    {
+        $query = new static::$query;
+        $bool = $query->delete($this->id);
+        return $bool;
+    }
+
+    /**
+     * Fill the model from an array with data
+     * @param Array $data   The data array to fill the attribute fields with
+     */
+    public function fill(array $parameters) : ModelContract
+    {
+        if (is_array($parameters)) {
+            foreach ($parameters as $key => $value) {
+                $this->setAttribute($key, $value);
+            }
+        }
+        // Fill rest with empty values
+        foreach ($this->fields as $key) {
+            if (!isset($this->vars[$key])) {
+                $this->setAttribute($key, null);
+            }
+        }
+        return $this;
+    }
+
+    /**
+     * Convert the model instance to an array
+     * @return Array
+     */
+    public function toArray() : array
+    {
+        $result = [];
+        if (is_array($this->fields)) {
+            foreach ($this->fields as $key) {
+                $result[$key] = $this->getAttribute($key);
+            }
+        }
+        return $result;
+    }
+
+    /**
+     * Get the loaded relations for ths model
+     * (for Fractal parseIncludes method)
+     * @return String
+     */
+    public function getIncludes() : Array
+    {
+        return array_keys($this->relations);
+    }
+
+    /**
+     * Get the model casts
+     * @return Array
+     */
+    public function getCasts() : array
+    {
+        return $this->casts;
+    }
+
+    /**
+     * Private method to convert a model attribute to the type defined in casts array
+     * @param string    $field
+     * @throws InvalidTypeException
+     * @return Mixed
+     */
+    private function convert($field, $value)
+    {
+        $validTypes = InvalidTypeException::getValidTypes();
+        if (is_array($this->casts) && in_array($field, array_keys($this->casts)) && !is_null($value)) {
+
+            $type = $this->casts[$field];
+
+            if (in_array($type, $validTypes)) {
+                switch ($type) {
+                    case 'string':
+                        $value = (string)$value;
+                        break;
+                    case 'integer':
+                    case 'int':
+                        $value = (int)$value;
+                        break;
+                    case 'float':
+                    case 'double':
+                    case 'real':
+                        $value = (float)$value;
+                        break;
+                    case 'bool':
+                    case 'boolean':
+                        $value = (boolean)$value;
+                        break;
+                    case 'array':
+                        $value = (array)$value;
+                        break;
+                    case 'unset':
+                        $value = null;
+                        break;
+                    case 'object':
+                        $value = (object)$value;
+                        break;
+                    case 'datetime':
+                        $value = Carbon::parse($value);
+                        break;
+                    default:
+                        $value = (string)$value;
+                }
+            } else {
+                throw new InvalidTypeException($type);
+            }
+        }
+        return $value;
+    }
+
+}
---
+++ src/Abstracts/Query.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace Halpdesk\Perform\Abstracts;
+
+use Illuminate\Support\Collection;
+use Halpdesk\Perform\Exceptions\ModelNotFoundException;
+use Halpdesk\Perform\Exceptions\QueryException;
+use Halpdesk\Perform\Exceptions\NotImplementedException;
+use Halpdesk\Perform\Contracts\Query as QueryContract;
+use Halpdesk\Perform\Contracts\Model as ModelContract;
+
+abstract class Query
+{
+    protected $collection;
+    protected $model;
+
+    /**
+     * Add a basic where clause to the query
+     * @throws QueryException
+     * @return QueryContract
+     */
+    public function where($key, $value) : QueryContract
+    {
+        // Must implement get() first
+        $this->collection = $this->get()->where($key, $value);
+        return $this;
+    }
+
+    /**
+     * Add an "order by" clause to the query
+     * @throws QueryException
+     * @return QueryContract
+     */
+    public function orderBy() : QueryContract
+    {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * Alias to set the "limit" value of the query
+     * @throws QueryException
+     * @return QueryContract
+     */
+    public function take() : QueryContract
+    {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * Execute query for a single record by id
+     * Return null if record with $id cannot be found
+     * @return ModelContract|null
+     */
+    public function find($id) : ?ModelContract
+    {
+        return $this->where('id', $id)->first();
+    }
+
+    /**
+     * Execute query for a single record by id
+     * @throws ModelNotFoundException
+     * @return ModelContract
+     */
+    public function findOrFail($id) : ModelContract
+    {
+        $model = $this->find($id);
+        if (!($model instanceof $this->model)) {
+            throw new ModelNotFoundException('not_found', $this->model, $id);
+        }
+        return $model;
+    }
+
+    /**
+     * Execute the query and return a collection
+     * @return Collection
+     */
+    public function get() : Collection
+    {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * Execute the query and get the first result
+     * Return null if record with $id cannot be found
+     * @return ModelContract|null
+     */
+    public function first() : ?ModelContract
+    {
+        return $this->get()->first();
+    }
+
+    /**
+     * Execute the query and get the first result
+     * @throws ModelNotFoundException
+     * @return ModelContract
+     */
+    public function firstOrFail() : ModelContract
+    {
+        $model = $this->first();
+        if (!($model instanceof $this->model)) {
+            throw new ModelNotFoundException('not_found', $this->model, null);
+        }
+        return $model;
+    }
+
+    /**
+     * Insert a record to the repository
+     * @throws QueryException
+     * @return ModelContract
+     */
+    public function create(array $parameters) : ModelContract
+    {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * Update records in the repository
+     * @throws QueryException
+     * @return ModelContract
+     */
+    public function update(array $parameters) : ModelContract
+    {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * Delte a record by its id in the repository
+     * @throws QueryException
+     * @return bool
+     */
+    public function delete($id) : bool
+    {
+        throw new NotImplementedException();
+    }
+
+}
