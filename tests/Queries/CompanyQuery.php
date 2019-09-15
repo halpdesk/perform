e93bc5aa6e1ae477713067fbd52d5881ed471cd8
Daniel Leppänen <daniel.leppanen@gmail.com>
2019-09-15 23:14:27 +0200

    Updated CompanyQuery in tests to create models for each row in get() method

--- tests/Queries/CompanyQuery.php
+++ tests/Queries/CompanyQuery.php
@@ -16,8 +16,13 @@ class CompanyQuery extends Query implements QueryContract
     public function get() : Collection
     {
         if (empty($this->collection)) {
-            $body = file_get_contents('./tests/data/companies.json');
-            $this->collection = new Collection(json_decode($body, true));
+            $body = file_get_contents('./tests/data/other-companies.json');
+            $rows = json_decode($body, true);
+            $companies = [];
+            foreach ($rows as $row) {
+                $companies[] = new $this->model($row);
+            }
+            $this->collection = collect($companies);
         }
         return $this->collection;
     }
